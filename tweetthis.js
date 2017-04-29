f6286be19b2976c773f2c22dbe3bf1800021d037
Pandelis Zembashis <info@pandelis.me>
2017-04-29 13:14:46 +0100

    Adapted the jQuery code to vanila JS
    Vanilla js implementation of jQuery onload, with support for IE8.
    
    Tested working on current example
    
    Issue: #1

--- tweetthis.js
+++ tweetthis.js
@@ -4,20 +4,34 @@
  * MIT Licence
  */
 
-$(document).ready (function(){
-  $("div.tweetthis").each(function(i, e) {
-    var text = $(e).text();
+var genTweetLink = function(text){
+  var html = '<a href="https://twitter.com/intent/tweet?text='
+  .concat(text)
+  .concat('" class="btn btn-tweetthis" target="_blank">')
+  .concat('<span class="fa fa-twitter"></span> Tweet This</a>')
 
-    if (text.length > 116) {
-      text = text.slice(0, 113) + "...";
+  return html
+}
+
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+
+      var tweets = document.querySelectorAll("div.tweetthis");
+
+      for(var t = 0; t < tweets.length; t++){
+
+        var text = tweets[t].textContent;
+        if (text.length > 116) {
+            text = text.slice(0, 113) + "...";
+        }
+        text = text + " " + window.location;
+        text = encodeURIComponent(text);
+
+        var newHtml = genTweetLink(text)
+
+        tweets[t].innerHTML += newHtml
+
+      }
     }
-    text = text + " " + window.location;
-    text = encodeURIComponent(text);
-    
-    var newHtml = $("<a class='btn btn-tweetthis' target='_blank'></a>")
-    .attr('href', "https://twitter.com/intent/tweet?text=" + text)
-    .html("<span class='fa fa-twitter'></span> Tweet This");
-
-    $(e).append(newHtml);
-  });
-});
+}
